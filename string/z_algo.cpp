feeeb7ad6411e49af8f050c87bc7df60944ac9b2
k82b <121232268+k82b@users.noreply.github.com>
2023-12-26 01:37:12 +0900

    Create z_algo.cpp

---
+++ string/z_algo.cpp
@@ -0,0 +1,15 @@
+template <class T> vector<int> zAlgo(T& S) {
+	int N = (int) size(S);
+	vector<int> A(N);
+	for (int i = 1, j = 0; i < N; ++i) {
+		if (i + A[i - j] < j + A[j]) {
+			A[i] = A[i - j];
+		} else {
+			A[i] = max(0, j + A[j] - i);
+			while (i + A[i] < N && S[A[i]] == S[i + A[i]]) ++A[i];
+			j = i;
+		}
+	}
+	A[0] = N;
+	return A;
+}
