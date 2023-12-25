d61feb0879833176d9010c0d0e59f3206372096a
k82b <121232268+k82b@users.noreply.github.com>
2023-12-26 00:17:09 +0900

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <any>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <new>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+using Int = long long;
+using Real = long double;
+template <class T> inline bool chmin(T& A, T B) { if (A > B) { A = B; return true; } else { return false; } }
+template <class T> inline bool chmax(T& A, T B) { if (A < B) { A = B; return true; } else { return false; } }
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+}
