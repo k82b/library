41d959db9b872af68e197fe159c182b37ebe2481
k82b <121232268+k82b@users.noreply.github.com>
2023-12-26 04:58:20 +0900

    Update unionfind.cpp

--- data_structure/unionfind/unionfind.cpp
+++ data_structure/unionfind/unionfind.cpp
@@ -1,26 +1,40 @@
-int root(vector<int>& uf, int u) {
-	return (uf[u] < 0) ? u : (uf[u] = root(uf, uf[u]));
-}
-bool connect(vector<int>& uf, int u, int v) {
-	u = root(uf, u);
-	v = root(uf, v);
-	if (u == v) return false;
-	if (uf[u] > uf[v]) swap(u, v);
-	uf[u] += uf[v];
-	uf[v] = u;
-	return true;
-}
-vector<vector<int>> groups(vector<int>& uf) {
-	int N = (int) size(uf);
-	vector<vector<int>> A(N);
-	for (int i = 0; i < N; ++i) {
-		A[root(uf, i)].push_back(i);
-	}
-	vector<vector<int>> B;
-	for (int i = 0; i < N; ++i) {
-		if (size(A[i])) {
-			B.push_back(A[i]);
+class UnionFind {
+	private:
+		int N;
+		vector<int> par;
+
+	public:
+		UnionFind() : UnionFind(0) {}
+		UnionFind(int N) : N(N), par(N, -1) {}
+		int root(int u) {
+			return (par[u] < 0) ? u : (par[u] = root(par[u]));
 		}
-	}
-	return B;
-}
+		bool connect(int u, int v) {
+			u = root(u);
+			v = root(v);
+			if (u == v) return false;
+			if (par[u] > par[v]) swap(u, v);
+			par[u] += par[v];
+			par[v] = u;
+			return true;
+		}
+		bool same(int u, int v) {
+			return root(u) == root(v);
+		}
+		int size(int u) {
+			return -par[root(u)];
+		}
+		vector<vector<int>> groups() {
+			vector<vector<int>> A(N);
+			for (int i = 0; i < N; ++i) {
+				A[root(i)].push_back(i);
+			}
+			vector<vector<int>> B;
+			for (int i = 0; i < N; ++i) {
+				if (!empty(A[i])) {
+					B.push_back(A[i]);
+				}
+			}
+			return B;
+		}
+};
