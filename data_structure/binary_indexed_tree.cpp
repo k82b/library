1da4cbc0fec6fce5077b79e5ec4ae81a37b761df
k82b <121232268+k82b@users.noreply.github.com>
2024-01-06 07:14:32 +0900

    Update binary_indexed_tree.cpp

--- data_structure/binary_indexed_tree.cpp
+++ data_structure/binary_indexed_tree.cpp
@@ -3,17 +3,17 @@ template <class T> struct BinaryIndexedTree {
 	vector<T> data;
 	BinaryIndexedTree() : BinaryIndexedTree(0) {};
 	explicit BinaryIndexedTree(int N) : N(N), data(N + 1) {}
+	void add(int k, T x) {
+		for (++k; k <= N; k += k & -k) {
+			data[k] += x;
+		}
+	}
 	T sum(int k) {
 		T ret = 0;
 		for (; k > 0; k -= k & -k) {
 			ret += data[k];
 		}
 		return ret;
-	}
-	void add(int k, T x) {
-		for (++k; k <= N; k += k & -k) {
-			data[k] += x;
-		}
 	}
 	T sum(int l, int r) {
 		return sum(r) - sum(l);
